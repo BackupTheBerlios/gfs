615722daa83bd5cd0016578d5fccc7fac42f1816
elefantenfloh <elefantenfloh>
2003-04-03 19:13:52 +0000

    update

--- www_public/index.php
+++ www_public/index.php
@@ -1,3 +1,5 @@
+<?php ?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/TR/xhtml1">
 <head>
@@ -6,21 +8,41 @@
 </head>
 
 <?php
-putenv ("LANG=de");
-bindtextdomain ("gfs", "./locale");
-textdomain ("gfs");
+
+$language=($HTTP_GET_VARS["language"]);
+
+if ($language=="") $language='English';
+
+include('functions');
+
+include('locale/English');
+include('locale/'.$language);
 ?>
+
 
 <body>
 
 <!-- Top-Logo -->
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
-<tr><td align="left"><img src="img/logo.gif" alt="GfS Logo"></td>
-<td></td>
-<td align="right" valign="top">
-<a href="http://developer.berlios.de">
-<img src="http://developer.berlios.de/bslogo.php?group_id=677" width="124" height="32" border="0" alt="BerliOS Logo"></a>
-</td></tr>
+	<tr>
+		<td align="left" rowspan="2"><img src="img/logo.gif" alt="GfS Logo"></td>
+		<td align="right" valign="top">
+			<a href="http://developer.berlios.de">
+			<img src="http://developer.berlios.de/bslogo.php?group_id=677" width="124" height="32" border="0" alt="BerliOS Logo"></a>
+		</td>
+	</tr>
+	<tr class="chooseLanguage">
+		<td>
+			<form method="get">
+				<?php echo $textLanguage ?>:&nbsp;
+				<select size="1" name="language">
+					<option <?php if ($language=="Deutsch") echo 'selected="selected"' ?>>Deutsch</option>
+					<option <?php if ($language=="English") echo 'selected="selected"' ?>>English</option>
+				</select>
+				&nbsp;<button type="submit"><?php echo $buttonChange ?></button>&nbsp;
+			</form>		
+		</td>
+	</tr>
 </table>
 
 
@@ -29,24 +51,18 @@ textdomain ("gfs");
 
 <!-- Menu left -->
 <tr><td valign="top" align="left">
-<table border="0" cellpadding="4" cellspacing="0">
+<table class="mainMenu">
 <?php
 
-function menuItem($n) {
-  echo '<tr><td class="menuItem"><a href="#" class="menuItem">'._($n).'</td></tr>';
-}
 
-function menuItemCurrent($n) {
-  echo '<tr><td class="menuItemCurrent">'._($n).'</td></tr>';
-}
-
-menuItemCurrent('page 1');
-menuItem('basic packages');
-menuItem('gnome base');
-menuItem('gnome core');
-menuItem('gadgets');
-menuItem('fifth toe');
-menuItem('applications');
+menuItemCurrent($textPage1);
+menuItem($textDocumentation,$language);
+menuItem($textBasicPackages,$language);
+menuItem($textGnomeCore,$language);
+menuItem($textGnomeExtra,$language);
+menuItem($text5thToe,$language);
+menuItem($text3rdParty,$language);
+menuItem($textNonGnome,$language);
 ?>
 </table>
 </td>
